52d74a819d83545a85af52940435ca55f4f48dfc
Natallia Radaman <Natallia_Radaman@epam.com>
2018-09-12 09:02:47 +0300

    Change build.gradle file
    Add layout files
    
    Add hard code content
    
    Add ArticlesRecyclerAdapter, ArticlesViewHolder classes and 2 interfaces: ArticleItemView, FragmentCoordinator
    
    Add 2 fragments: FragmentArticleRecyclerView, FragmentArticleDetail. Edit MaimActivity
    
    Change build.gradle file
    
    Add layout files
    
    Add hard code content
    
    Add ArticlesRecyclerAdapter, ArticlesViewHolder classes and 2 interfaces: ArticleItemView, FragmentCoordinator
    
    Add 2 fragments: FragmentArticleRecyclerView, FragmentArticleDetail. Edit MaimActivity
    
    CAPDEV-26689: RecyclerView
    
    RecyclerView with layout edits
    
      - Add layouts according lint warnings
    
    CAPDEV-26689: RecyclerView
    
    Edit hard code content
    
      - edit HardCodeContent/HardCodeContent and HardCodeContent/HardCodeArticle according lint warnings
    
    CAPDEV-26689: RecyclerView
    
    Edit recycler view
    
      - edit ArticlesRecyclerAdapter according lint warnings
      - edit ArticlesViewHolder according lint warnings
    
    CAPDEV-26689: RecyclerView
    
    Edit files accordind lint warnings
    
      - edit 2 fragments: FragmentArticleDetail and FragmentArticleRecyclerView
      - edit MainActivity
      - edit ArticlesRecyclerAdapter
      - edit HardCodeContent/HardCodeContent
    
    CAPDEV-26689: Recycler View according Pavel's github comment
    
    Edit recyclerView
    
      - Delete 2 fragments (fragment_article_details, fragment_article_recycler) and FragmentCoordinator interface.
      - Add RecyclerTouchItemListener class which implements RecyclerView.OnItemTouchListener.
    
    CAPDEV-26689: RecyclerView
    
    RecyclerView with two classes
    
      - Refactor code
      - Delete unnecessary classes
    
    CAPDEV-26689: RecyclerView
    
      - Edit layout/aritcle_item_view
    
    CAPDEV-26689: RecyclerView
    
    Change parent LinearLayout to CardView for click working on the whole place for view
    
      - Add CardView

--- app/src/main/java/com/example/natallia_radaman/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/natallia_radaman/simplerecyclerview/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.natallia_radaman.simplerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * An activity representing a list of Articles.
+ */
+public class MainActivity extends AppCompatActivity implements ArticlesRecyclerAdapter.RecyclerViewOnItemClickListener {
 
+    private static final int NUMBER_OF_ARTICLES = 30;
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeArticleTitles();
+
+        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        final ArticlesRecyclerAdapter adapter = new ArticlesRecyclerAdapter(makeArticleTitles());
+        adapter.setRecyclerViewOnItemClickListener(this);
+
+        final RecyclerView recyclerView = findViewById(R.id.article_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private String[] makeArticleTitles() {
+        final String[] articleTitles = new String[NUMBER_OF_ARTICLES];
+
+        for (int i = 0; i < NUMBER_OF_ARTICLES; i++) {
+            articleTitles[i] = "Article number " + i;
+        }
+
+        return articleTitles;
+    }
+
+    @Override
+    public void onItemClick(final String item) {
+        Toast.makeText(this, "You clicked on " + item.toLowerCase() + ".", Toast.LENGTH_SHORT).show();
     }
 }
